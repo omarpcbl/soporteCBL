3dc0902f164f976f28e6577e46aa525c0a4d301b
Omar <oawind@gmail.com>
2023-09-22 15:08:07 -0300

    -Se agregó recuperarCae masivo (masivo de verdad). Tener en consideración no bombardear el servidor con la cantidad de facturas a procesar -se agregó un ejemplo Facturas para listas las facturas a recuperar y reemplazar el cuit del cliente -se agrego otro archivo json en caso de que se desee hacer pruebas en entornos locales. Descomentar la línea de import en el archivo principal

---
+++ recuperarCAE/ejemploFacturas.js
@@ -0,0 +1,19 @@
+const facturas = [
+  {
+    Tipo: "FCA",
+    Punto: "10",
+    Numero: "3279",
+  },
+  {
+    Tipo: "FCA",
+    Punto: "10",
+    Numero: "3284",
+  },
+  {
+    Tipo: "FCA",
+    Punto: "10",
+    Numero: "3358",
+  },
+];
+
+let cuit = "30684766690";
---
+++ recuperarCAE/recuperarCaeMasivo.js
@@ -0,0 +1,77 @@
+//import { cuit, facturas } from "./facturas.json";
+
+async function rCae(facturas, cuit) {
+  let detenerBucle = false; // Variable de bandera para detener el bucle
+
+  for (const factura of facturas) {
+    if (detenerBucle) {
+      break; // Salir del bucle si la bandera está establecida
+    }
+
+    const [cuitUsuario, tipoComprobante, punto, nroComprobante, generarNC] =
+      document.body.querySelectorAll(
+        "#txtNroComprobante, #txtCuitUsuario, #txtPunto, #ddlTipoComprobante, #chkGenerarNC"
+      );
+
+    nroComprobante.value = factura.Numero;
+    cuitUsuario.value = cuit;
+    punto.value = factura.Punto;
+    tipoComprobante.value = factura.Tipo;
+    generarNC.checked = "true";
+
+    // Llamar a recuperarCAE() y pasar un callback para controlar el flujo
+    await new Promise((resolve) => {
+      recuperarCAE((resultado) => {
+        if (resultado === false) {
+          detenerBucle = true; // Establecer la bandera para detener el bucle
+        }
+        resolve();
+      });
+    });
+  }
+}
+
+function recuperarCAE(callback) {
+  $("#divError,#divOk").hide();
+  if (
+    $(
+      "#txtNroComprobante,#txtCuitUsuario,#txtPunto,#ddlTipoComprobante"
+    ).valid()
+  ) {
+    Common.mostrarProcesando("btnActualizar");
+    $("#divError").hide();
+    var info =
+      "{ nroComprobante: '" +
+      $("#txtNroComprobante").val() +
+      "', cuitUsuario: '" +
+      $("#txtCuitUsuario").val() +
+      "', punto: " +
+      $("#txtPunto").val() +
+      ", tipoComprobante: '" +
+      $("#ddlTipoComprobante").val() +
+      "', generarNC: " +
+      $("#chkGenerarNC").is(":checked") +
+      " }";
+
+    $.ajax({
+      type: "POST",
+      url: "/Soporte/RecuperarCae",
+      data: info,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (data) {
+        Common.ocultarProcesando("btnActualizar", "Actualizar");
+        if (data.TieneError) {
+          $("#msgError").html(data.Mensaje);
+          $("#divError").show();
+          callback(false); // Llama al callback con 'false' para indicar un error
+        } else {
+          $("#divOk").show();
+          callback(true); // Llama al callback con 'true' para indicar éxito
+        }
+      },
+    });
+  } else {
+    callback(false); // Llama al callback con 'false' si los datos no son válidos
+  }
+}
